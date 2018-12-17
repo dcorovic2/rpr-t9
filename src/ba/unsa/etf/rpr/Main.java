d83122158241d6b9fd2e345ca323489830273a8e
Dalila Corovic <dcorovic2@etf.unsa.ba>
2018-12-17 23:30:29 +0100

    Proba

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -6,7 +6,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        String url = "jdbc:sqlite:resources/baza.db";
+
+/*
+
         System.out.println("Gradovi su:\n" + ispisiGradove());
-        glavniGrad();
+        glavniGrad();*/
     }
 }
