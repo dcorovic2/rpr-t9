a2e5b052228838f86e63efddbfb9f9886d236b06
Dalila Corovic <dcorovic2@etf.unsa.ba>
2018-12-18 14:16:45 +0100

    GeografijaDAO

--- src/ba/unsa/etf/rpr/GeografijaDAO.java
+++ src/ba/unsa/etf/rpr/GeografijaDAO.java
@@ -1,12 +1,51 @@
 package ba.unsa.etf.rpr;
 
+import java.sql.*;
+
 public class GeografijaDAO {
     private static GeografijaDAO ourInstance = new GeografijaDAO();
+    private Connection conn;
 
     public static GeografijaDAO getInstance() {
         return ourInstance;
     }
 
     private GeografijaDAO() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:sqlite/baza.db");
+
+            String upit1 = "INSER INTO grad(id, naziv, broj_stanovnika, drzava) " +
+                    "VALUES (1, Pariz, 2200000, Francuska)";
+
+            String upit2 = "INSER INTO grad(id, naziv, broj_stanovnika, drzava) " +
+                    "VALUES (2, London, 8136000, United Kingdom)";
+
+            String upit3 = "INSER INTO grad(id, naziv, broj_stanovnika, drzava) " +
+                    "VALUES (3, Bec, 1868000, Austija)";
+
+            String upit4 = "INSER INTO grad(id, naziv, broj_stanovnika, drzava) " +
+                    "VALUES (4, Manchester, 510746, United Kingdom)";
+
+            String upit5 = "INSER INTO grad(id, naziv, broj_stanovnika, drzava) " +
+                    "VALUES (5, Graz,  283869, Austrija)";
+
+            Statement stmt = conn.createStatement();
+            stmt.executeQuery(upit1);
+            stmt.executeQuery(upit2);
+            stmt.executeQuery(upit3);
+            stmt.executeQuery(upit4);
+            stmt.executeQuery(upit5);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+/*
+    public Grad glavniGrad(String drzava) {
+
+    }*/
+
 }
